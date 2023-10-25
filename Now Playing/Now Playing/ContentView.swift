e1f91e02b5bf1dab4196461079384d3da9315661
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2023-10-25 14:56:37 -0400

    created the xcode project

---
+++ Now Playing/Now Playing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Now Playing
+//
+//  Created by Brandon Lamer-Connolly on 10/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Now Playing/Now Playing/Now_PlayingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Now_PlayingApp.swift
+//  Now Playing
+//
+//  Created by Brandon Lamer-Connolly on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Now_PlayingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
