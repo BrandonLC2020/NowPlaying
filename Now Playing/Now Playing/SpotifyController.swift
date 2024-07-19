cccc416b69b659ac72885d33cda7697a3e46ef0e
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-07-19 19:57:43 -0400

    lots and lots of changes for setup

---
+++ Now Playing/Now Playing/SpotifyController.swift
@@ -0,0 +1,131 @@
+//
+//  SpotifyController.swift
+//  Now Playing
+//
+//  Created by Brandon Lamer-Connolly on 7/19/24.
+//
+
+import SwiftUI
+import SpotifyiOS
+import Combine
+
+@MainActor
+final class SpotifyController: NSObject, ObservableObject {
+    let spotifyClientID = "4fd5ff43ca794b528e96cf30eb097819"
+    let spotifyRedirectURL = URL(string:"spotify-ios-quick-start://spotify-login-callback")!
+    
+    var accessToken: String?
+    @Published var currentTrackURI: String?
+    @Published var currentTrackName: String?
+    @Published var currentTrackArtist: String?
+    @Published var currentTrackDuration: Int?
+    @Published var currentTrackImage: UIImage?
+    
+    private var connectCancellable: AnyCancellable?
+        
+    private var disconnectCancellable: AnyCancellable?
+
+    func setAccessToken(from url: URL) {
+        let parameters = appRemote.authorizationParameters(from: url)
+        
+        if let accessToken = parameters?[SPTAppRemoteAccessTokenKey] {
+            appRemote.connectionParameters.accessToken = accessToken
+            self.accessToken = accessToken
+        } else if let errorDescription = parameters?[SPTAppRemoteErrorDescriptionKey] {
+            // Handle the error
+        }
+    }
+    
+    func authorize() {
+        self.appRemote.authorizeAndPlayURI("")
+    }
+    
+    lazy var configuration = SPTConfiguration(
+        clientID: spotifyClientID,
+        redirectURL: spotifyRedirectURL
+    )
+
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.connectionParameters.accessToken = self.accessToken
+        appRemote.delegate = self
+        return appRemote
+    }()
+
+    func connect() {
+        if let _ = self.appRemote.connectionParameters.accessToken {
+            appRemote.connect()
+        }
+    }
+
+    func disconnect() {
+        if appRemote.isConnected {
+            appRemote.disconnect()
+        }
+    }
+    
+    override init() {
+        super.init()
+        connectCancellable = NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)
+            .receive(on: DispatchQueue.main)
+            .sink { _ in
+                self.connect()
+            }
+        
+        disconnectCancellable = NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)
+            .receive(on: DispatchQueue.main)
+            .sink { _ in
+                self.disconnect()
+            }
+    }
+    
+    func fetchImage() {
+        appRemote.playerAPI?.getPlayerState { (result, error) in
+            if let error = error {
+                print("Error getting player state: \(error)")
+            } else if let playerState = result as? SPTAppRemotePlayerState {
+                self.appRemote.imageAPI?.fetchImage(forItem: playerState.track, with: CGSize(width: 300, height: 300), callback: { (image, error) in
+                    if let error = error {
+                        print("Error fetching track image: \(error.localizedDescription)")
+                    } else if let image = image as? UIImage {
+                        DispatchQueue.main.async {
+                            self.currentTrackImage = image
+                        }
+                    }
+                })
+            }
+        }
+    }
+}
+
+extension SpotifyController: SPTAppRemoteDelegate {
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        self.appRemote = appRemote
+        self.appRemote.playerAPI?.delegate = self
+        self.appRemote.playerAPI?.subscribe(toPlayerState: { (result, error) in
+            if let error = error {
+                print("Error subscribing to player state: \(error.localizedDescription)")
+            } else {
+                print("Successfully subscribed to player state")
+            }
+        })
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        // Handle the connection failure
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        // Handle the connection loss
+    }
+}
+
+extension SpotifyController: SPTAppRemotePlayerStateDelegate {
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        self.currentTrackURI = playerState.track.uri
+        self.currentTrackName = playerState.track.name
+        self.currentTrackArtist = playerState.track.artist.name
+        self.currentTrackDuration = Int(playerState.track.duration) / 1000 // playerState.track.duration is in milliseconds
+        fetchImage()
+    }
+}
