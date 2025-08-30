2a2bf88a118668d18aaf8a55d551b4a3c2d1f2fa
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-08-30 15:53:26 -0400

    renamed apple watch target

---
+++ Now Playing/watchOS Watch App/watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchOSApp.swift
+//  watchOS Watch App
+//
+//  Created by Brandon Lamer-Connolly on 8/30/25.
+//
+
+import SwiftUI
+
+@main
+struct watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Now Playing/watchOS Watch AppTests/watchOS_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  watchOS_Watch_AppTests.swift
+//  watchOS Watch AppTests
+//
+//  Created by Brandon Lamer-Connolly on 8/30/25.
+//
+
+import Testing
+@testable import watchOS_Watch_App
+
+struct watchOS_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
